bd87c78dd94b2fd6ea3606453d77d7a2108fdd6d
Anfimov <anfimov.dev@gmail.com>
2018-01-29 13:26:46 +0200

    added test for SetOptions

--- ingest/operation_details.go
+++ ingest/operation_details.go
@@ -63,6 +63,8 @@ func setOperationDetails(source string, operation xdr.OperationBody,
 	case xdr.OperationTypeSetOptions:
 		op := operation.MustSetOptionsOp()
 
+		operationDetails.SetOptions = &history.SetOptionsDetails{}
+
 		if op.MasterWeight != nil {
 			operationDetails.SetOptions.MasterKeyWeight = (*uint32)(op.MasterWeight)
 		}
